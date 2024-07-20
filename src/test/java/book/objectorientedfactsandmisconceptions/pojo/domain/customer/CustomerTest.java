96e76de83ed75d3ab6f0fa40e2d9a50e4e36be12
SsangSoo <kss10080@naver.com>
2024-07-20 23:48:51 +0900

    testcode complete only one

--- src/test/java/book/objectorientedfactsandmisconceptions/pojo/domain/customer/CustomerTest.java
+++ src/test/java/book/objectorientedfactsandmisconceptions/pojo/domain/customer/CustomerTest.java
@@ -16,9 +16,9 @@ class CustomerTest {
     @Test
     void getPaymentHistoryAtDay() {
         //given
-        Customer todtn = Customer.createCustomer("쌩수");                     // 고객
-        CoffeeOrder threeAmericano = CoffeeOrder.of("아메리카노", 3);    // 주문할 커피
-        todtn.orderCoffee(List.of(threeAmericano), "쌩스타");              // 커피 주문
+        Customer todtn = Customer.createCustomer("쌩수");                         // 고객
+        CoffeeOrder threeAmericano = CoffeeOrder.of("아메리카노", 3);        // 주문할 커피
+        todtn.orderCoffee(List.of(threeAmericano), "쌩스타");                 // 커피 주문
 
         //when
         PaymentHistory paymentHistoryAtDay = todtn.getPaymentHistoryAtDay(LocalDate.now());
@@ -26,11 +26,11 @@ class CustomerTest {
         List<OrderHistory> orderHistoryList = paymentHistoryAtDay.getOrderHistoryList();
 
         //then
-        assertThat(orderHistoryList.size()).isEqualTo(1);
+        assertThat(orderHistoryList.size()).isEqualTo(1);                       // 한 개의 주문내역
 
-//        OrderHistory orderHistory = orderHistoryList.getFirst();
-//        assertThat(orderHistory).isExactlyInstanceOf()
-//                .
+        OrderHistory orderHistory = orderHistoryList.getFirst();                        // 주문 내역 안에 12000원과 주문한 날짜(오늘)
+        assertThat(orderHistory).extracting("orderedDate", "totalPrice")
+                .containsExactlyInAnyOrder(LocalDate.now(), 12000);
     }
 
 }
