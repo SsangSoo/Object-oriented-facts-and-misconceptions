1f65b8c8d907552efe0168abc838aca23b69022f
SsangSoo <kss10080@naver.com>
2024-07-22 09:38:53 +0900

    customer order testing

--- src/test/java/book/objectorientedfactsandmisconceptions/pojo/domain/customer/CustomerTest.java
+++ src/test/java/book/objectorientedfactsandmisconceptions/pojo/domain/customer/CustomerTest.java
@@ -4,6 +4,8 @@ import book.objectorientedfactsandmisconceptions.pojo.domain.CoffeeOrder;
 import book.objectorientedfactsandmisconceptions.pojo.domain.Customer;
 import book.objectorientedfactsandmisconceptions.pojo.domain.OrderHistory;
 import book.objectorientedfactsandmisconceptions.pojo.domain.PaymentHistory;
+import book.objectorientedfactsandmisconceptions.pojo.domain.coffee.Menu;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -14,6 +16,7 @@ import static org.assertj.core.api.Assertions.*;
 class CustomerTest {
 
     @Test
+    @DisplayName("커피를 주문한 내역을 확인할 수 있다.")
     void getPaymentHistoryAtDay() {
         //given
         Customer todtn = Customer.createCustomer("쌩수");                         // 고객
@@ -31,6 +34,14 @@ class CustomerTest {
         OrderHistory orderHistory = orderHistoryList.getFirst();                        // 주문 내역 안에 12000원과 주문한 날짜(오늘)
         assertThat(orderHistory).extracting("orderedDate", "totalPrice")
                 .containsExactlyInAnyOrder(LocalDate.now(), 12000);
+
+        assertThat(orderHistory.getCoffeeOrderList().size()).isEqualTo(1);
+        CoffeeOrder coffeeOrder = orderHistory.getCoffeeOrderList().getFirst();
+        assertThat(coffeeOrder).extracting("menu", "count")
+                .containsExactlyInAnyOrder(Menu.AMERICANO, 3);
+
+        assertThat(coffeeOrder.getMenu()).extracting("coffee", "price")
+                .containsExactlyInAnyOrder("아메리카노", 4000);
     }
 
 }
